b19267a1be0f5c5c3bba2fdaaa70d26d90da8a28
Jacob Ramos <97527427+JHudRam@users.noreply.github.com>
2023-03-31 08:58:22 -0600

    Add files via upload

---
+++ number.cpp
@@ -0,0 +1,34 @@
+/*
+ PIC 10A Homework 1, number.cpp
+ Author: Jacob Ramos
+ Date: 1/27/23
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int num;
+    cout << "Input a three-digit number (100 - 999):" << endl;
+    cin >> num;
+    
+    //Reverse of the number
+    int tempnum = num;
+    int lastdigit = tempnum % 10;
+    num /= 10;
+    int seconddigit = tempnum % 10;
+    num /= 10;
+    int firstdigit = tempnum;
+    int reverse = lastdigit*100 + seconddigit*10 + firstdigit;
+    cout << "The reverse of the number is " << reverse << "." << endl;
+    
+
+    
+    //Triple the number
+    int triple;
+    triple = tempnum*3;
+    cout << "The triple of the number is " << triple << "." << endl;
+    
+    return 0;
+}
